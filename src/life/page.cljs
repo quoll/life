1e1cad1264ac4d931e128c810a499d3337200c19
Paula Gearon <pgearon@cisco.com>
2018-05-22 00:25:30 -0400

    Bugfix on timers, and expanded field

--- src/life/page.cljs
+++ src/life/page.cljs
@@ -4,8 +4,6 @@
             [life.core :refer [life]]))
 
 (def sq "size of a square" 25)
-(def default-width 700)
-(def default-height 300)
 (def life-color "blue")
 
 (defn next-life
@@ -23,21 +21,24 @@
   [app-state x y v]
   (swap! app-state update :data #(mset % y x (- 1 v)))
   (when (:finished @app-state)
-    (swap! app-state dissoc :finished)
-    (js/setInterval (next-life app-state) 125)))
+    (swap! app-state assoc
+           :finished false
+           :timer (js/setInterval (next-life app-state) 125))))
 
 (defn field
   [app-state]
   (let [data (:data @app-state)
-        [height width] (shape data)]
-    [:svg {:width (+ 2 default-width) :height (+ 2 default-height)}
-     [:rect {:x 0 :y 0 :width (inc default-width) :height (inc default-height)
+        [height width] (shape data)
+        pixel-width (* sq width)
+        pixel-height (* sq height)]
+    [:svg {:width (+ 2 pixel-width) :height (+ 2 pixel-height)}
+     [:rect {:x 0 :y 0 :width (inc pixel-width) :height (inc pixel-height)
              :style {:stroke "black"
                      :stroke-width 1
                      :fill "white"}}]
      (vec
       (concat
-       [:svg {:width default-width :height default-height :x 1 :y 1}]
+       [:svg {:width pixel-width :height pixel-height :x 1 :y 1}]
        (for [x (range width) y (range height) :let [v (mget data y x)]]
          [:rect {:x (* x sq) :y (* y sq) :width sq :height sq
                  :style {:fill (if (zero? v) "white" "blue")
